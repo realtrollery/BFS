145ee3fc42f91dc7e920eab798a487bcc00b18ab
Doyoon Yi <93254979+realtrollery@users.noreply.github.com>
2022-03-05 10:34:07 +0900

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -25,20 +25,3 @@ q = deque()
 x = 10
 y = 8
 chunk = 0
-grid = []
-trueZone = 17
-coordinates = [[0, 0], [1, 0], [1, 1], [4, 2], [4, 3], [4, 5], [2, 4], [3, 4], [7, 4], [8, 4], [9, 4], [7, 5], [8, 5],
-               [9, 5], [7, 6], [8, 6], [9, 6]]
-for i in range(y):
-    grid.append([0] * x)
-for i2 in range(trueZone):
-    grid[coordinates[i2][1]][coordinates[i2][0]] = 1
-print(grid)
-for y1 in range(y):
-    for x1 in range(x):
-        if grid[y1][x1] == 1:
-            BFS(x1, y1)
-            print(grid)
-            chunk += 1
-print(grid)
-print(chunk)
